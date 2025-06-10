36dd48a32d0dbc298d77c5237085fdce6ba6f4cc
rafael <silva.rafa@hotmail.com>
2025-06-10 18:05:38 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,23 +1,159 @@
-const Database = require('./src/controller/sqlite/index')
+const express = require("express");
+const app = express()
 
+
+const Database = require('./src/controller/sqlite/index')
+app.use(express.json())
 /** * INSERT */
-// Database.usuario.insert({ user: 'zacarias', password: '546465165165156' })
-//     .then(response => { console.log({ response }) })
-//     .catch(error => { console.error(error) })
+
+app.post("/cadastrar", async (req, res) => {                                                        //não conseguimos aplicar o toLawerCase
+    try {
+        const { user, password } = req.body
+        const usuarioMinusculo = user.toLowerCase()
+        if (!user || !password) return res.status(422).json({ mens: "Password e user precisam ser informados" })
+        const cadastro = await Database.usuario.insert(
+            usuarioMinusculo,
+            password
+        )
+        return res.status(200).json({
+            mens: "Usuário Cadastrado ",
+            "Dados": {
+                "user": user,
+                "password": password,
+            }
+        })
+    } catch (e) {
+        return res.status(409).json({ mens: "Usuário ja existe" })
+    }
+
+})
+
+
+app.post("/login", async (req, res) => {
+    try {
+        const { user, password } = req.body
+
+        if (!user || !password) res.status(422).json({ mens: "Nome e senha precisam ser informados" })
+
+        const retorno = await Database.usuario.login({
+            user,
+            password
+        })
+
+        return res.status(200).json({
+            mens: "Usuário Logado "
+        })
+    } catch (e) {
+        console.log(e)
+        if (e.status == 400) return res.status(401).json({ mens: "Usuário ou senha inválidos" })
+        return res.status(500).json({ mens: "Erro do servidor" })
+    }
+})
+
+
+app.delete("/delete", async (req, res) => {                                                              //usuario é deletado , mais cai diretamente no erro
+    try {
+        const { user } = req.body
+
+        if (!user) return res.status(422).json({ mens: "Usuário precisa ser informado" })
+
+
+        const retorno = await Database.usuario.delete({
+            user
+        })
+
+    } catch (e) {
+        return res.status(400).json({ mens: "Usuario não existe" })
+    }
+    return res.status(200).json({
+        mens: "Usuário Deletado "
+    })
+})
+
+app.put("/update", async (req, res) => {
+    try {
+        const { user, password } = req.body
+
+        if (!user || !password) return res.status(422).json({ mens: "Id , user e password precisam ser informados" })
+
+        const retorno = await Database.usuario.update({
+            user,
+            password
+        })
+        if (!retorno.updated) return res.status(400).json({ mens: "Usuario não existe" })
+    } catch (e) {                                                                                       
+        return res.status(500).json({ mens: "Erro Geral" })
+    }
+    return res.status(200).json({
+        mens: "Usuário Atualizado"
+    })
+})
+
+
+app.get("/buscarTodos", async (req, res) => {
+    const usuarios = await Database.usuario.selectAll()
+    const listaUsuarios = usuarios.map((item) => {
+        return ({
+            "Id": item.id,
+            "User": item.user,
+            "Password": item.password
+        })
+    })
+
+    return res.status(200).json({ "Lista de Usuarios": listaUsuarios })
+})
+
+app.get("/buscarUm", async (req, res) => {
+    try {
+        //   throw new Error("qualquer")                                                                                             //como trazer os outros dados
+        const { user } = req.body
+
+        if (!user) return res.status(422).json({ mens: "User precisa ser informado" })
+
+        const usuario = await Database.usuario.selectUser(
+            user,
+        )
+
+        return res.status(200).json({
+            mens: "Usuário Escolhido ",
+            "Dados": {
+                "User": usuario,
+
+            }
+        })
+    } catch (e) {
+        if (e.status == 400) return res.status(400).json({ mens: "Usuário não existe" })
+
+        return res.status(500).json({ mens: "Erro Geral" })
+    }
+
+})
+
+
+
+
+//Database.usuario.insert({ user: 'zacarias', password: '546465165165156' })
+//   .then(response => { console.log({ response }) })
+//   .catch(error => { console.error(error) })
 
 /** * SELECT ALL */
-Database.usuario.selectAll()
-    .then(response => { console.log({ response }) })
-    .catch(error => { console.error(error) })
+//Database.usuario.selectAll()
+//    .then(response => { console.log({ response }) })
+//    .catch(error => { console.error(error) })
 
 
 /** * LOGIN */
-// Database.usuario.login({ user: 'rodrigo', password: '123456' })
-//     .then(response => { console.log({ response }) })
-//     .catch(error => { console.error(error) })
+//Database.usuario.login({ user: 'rodrigo', password: '123456' })
+//    .then(response => { console.log({ response }) })
+//    .catch(error => { console.error(error) })
 
 
 /** * DELETE */
-// Database.usuario.delete({ id: 2 })
-//     .then(response => { console.log({ response }) })
-//     .catch(error => { console.error(error) })
+//Database.usuario.delete({ id: 9 })
+//    .then(response => { console.log({ response }) })
+//    .catch(error => { console.error(error) })
+
+
+app.listen(8000, () => {
+    console.log("backend rodando")
+})
--- src/controller/sqlite/tables/usuario.js
+++ src/controller/sqlite/tables/usuario.js
@@ -1,15 +1,16 @@
 'use strict'
 
 const sqlite3 = require('sqlite3').verbose()
+const { error } = require('console')
 const path = require('path')
 
 class Table {
     constructor() {
-        this.schema = `.mydbsql`
-        this.pathdatabase = path.join(__dirname, '../', this.schema)
-        this.table = 'usuario'
-    }
-
+        this.schema = `mydbsql`                                                                                     
+        this.pathdatabase = path.join(__dirname, '../', this.schema)                                                 //ver essa questão na aula
+        this.table = 'usuario'                                                                                       
+    }                                                                                                                //questão do erro no reject a melhor forma de se trabalhar é esta?
+                                                                                                                     //se ele usaria promise para fazer essa interface
     Db() {
         return new Promise((resolve, reject) => {
             const db = new sqlite3.Database(this.pathdatabase)
@@ -23,7 +24,7 @@ class Table {
         })
     }
 
-    insert({ user, password }) {
+    insert(user, password) {
         return new Promise((resolve, reject) => {
             this.Db().then(db => {
                 db.run(`INSERT INTO ${this.table}(user, password) VALUES(?,?)`, [user, password], (err) => {
@@ -35,39 +36,53 @@ class Table {
         })
     }
 
-    login({ user, password }) {
+    login( {user, password} ) {
         return new Promise((resolve, reject) => {
             this.Db().then(db => {
                 db.get(`SELECT * FROM ${this.table} WHERE user = ? AND password = ?`, [user, password], (err, rows) => {
                     db.close()
                     if (err) reject(err)
+
+                    else if (!rows) {
+                        //                throw new Error("qualquer")
+                        console.error("Deu erro")
+                        reject({ status: 400 })
+                        //                          resolve({error: "deu erro"})
+                    }
                     else resolve(rows)
                 })
-            }).catch(error => reject(error))
+            })
         })
     }
 
-    update({ id, user, password }) {
+    update( {user, password} ) {
         return new Promise((resolve, reject) => {
             this.Db().then(db => {
-                db.run(`UPDATE ${this.table} SET user = ?, password = ? WHERE id = ?`, [user, password, id], (err) => {
+                db.get(`SELECT * FROM ${this.table} WHERE user = ? `, [user, password], (err, rows) => {
+                    //db.close()
+                    if (!rows) resolve({ updated: false })
+                })
+                db.run(`UPDATE ${this.table} SET password = ? WHERE user = ?`, [password, user], (err) => {
                     db.close()
                     if (err) reject(err)
-                    else resolve({ table: this.table, id, message: `Atualizado com sucesso` })
+                    else resolve({ table: this.table, user, message: `Atualizado com sucesso`, updated: true })
                 })
             }).catch(error => reject(error))
         })
     }
 
-    delete({ id }) {
+    delete( {user} ) {
         return new Promise((resolve, reject) => {
             this.Db().then(db => {
-                db.run(`DELETE FROM ${this.table} WHERE id = ?`, [id], (err) => {
+                db.run(`DELETE FROM ${this.table} WHERE user = ?`, [user], (err, rows) => {
                     this.close
                     if (err) reject(err)
-                    else resolve({ table: this.table, id, message: `Removido com sucesso` })
+                        else if (!rows) {
+                            reject({ status: 400 })
+                        }
+                    else resolve({ table: this.table, user, message: `Removido com sucesso` })
                 })
-            }).catch(error => reject(error))
+            })
         })
     }
 
@@ -82,14 +97,19 @@ class Table {
         })
     }
 
-    selectWhere({ user, password }) {
+    selectUser( user ) {
         return new Promise((resolve, reject) => {
             this.Db().then(db => {
-                db.all(`SELECT * FROM ${this.table} WHERE user=? AND password=? ORDER BY id;`, [user, password], (error, rows) => {
-                    if (error) { console.error(error) }
-                    resolve(rows)
+                db.get(`SELECT  id, user FROM ${this.table} WHERE user = ?`, [user], (err, rows) => {
+                    this.close
+                    if (err) reject(err)
+                        else if (!rows) {
+                            reject({ status: 400 })
+                        }
+                    else resolve( rows )
+
                 })
-            })
+            }).catch(error => reject(error))
         })
     }
 
